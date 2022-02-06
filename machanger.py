e6e938983985891218d2ed6a0266bc4e946e0c65
CodePrefer <98956167+CodePrefer@users.noreply.github.com>
2022-02-06 03:41:20 -0500

    Update machanger.py

--- machanger.py
+++ machanger.py
@@ -2,19 +2,28 @@
 
 import subprocess
 import optparse
+import re
 
-parser = optparse.OptionParser()
-parser.add_option("-i","--interface",dest="interface", help="Interface name for changing Mac address")
-parser.add_option("-m","--mac_address",dest="new_mac", help="New Mac address for selected interface")
-(options,arguments)=parser.parse_args()
+def optionParameters():
+	parser = optparse.OptionParser()
+	parser.add_option("-i","--interface",dest="interface", help="Enter interfce which you want to change")
+	parser.add_option("-m","--mac_address",dest="new_mac", help="For New Mac Address")
+	(options,arguments)=parser.parse_args()
+	return options
 
-interface = options.interface
-mac = options.new_mac
+
+parameters = optionParameters()
+
+interface = parameters.interface
+mac = parameters.new_mac
 
 if not interface:
-    print("please show help")
+    print("Please check help using '--help'")
 else:
     subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", mac])
+    subprocess.call(["ifconfig", interface,"hw","ether", mac])
     subprocess.call(["ifconfig", interface, "up"])
+    New_Mac_add = str(subprocess.check_output(["ifconfig", interface]))
+    show = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", New_Mac_add)
+    print("The new MAC address has been updated. ", "\nMac_Address :", show.group(0))
     print("End ............")
